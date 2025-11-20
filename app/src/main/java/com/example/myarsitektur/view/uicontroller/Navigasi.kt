e48ab081b32481396d20a2954b607d1ef8c5d247
mahasiswa229 <mafaqih1945@gmail.com>
2025-11-20 15:30:57 +0700

    feat: Add detail screen route to NavHost
    Adds a new `composable` to the `NavHost` for the detail screen, using the `Navigasi.Detail.name` route.
    This route displays the `TampilSiswa` composable, passing in the UI state and an `onBackButtonClicked` handler.

--- app/src/main/java/com/example/myarsitektur/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/myarsitektur/view/uicontroller/Navigasi.kt
@@ -35,6 +35,14 @@ fun SiswaApp(
                     }
                 )
             }
-            
+            composable(route = Navigasi.Detail.name) {
+                TampilSiswa(
+                    statusUiSiswa = uiState.value,
+                    onBackButtonClicked = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
+        }
     }
 }
