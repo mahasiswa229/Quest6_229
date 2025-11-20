33282b4ef98efa5f4530c43825738ee53c7348e7
mahasiswa229 <mafaqih1945@gmail.com>
2025-11-20 15:00:37 +0700

    feat: Add FormSiswa composable
    Adds the `FormSiswa` composable function. This function takes a list of gender options, an `onSubmitButtonClicked` callback, and a modifier.

--- app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
@@ -2,3 +2,8 @@ package com.example.myarsitektur.view
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+fun FormSiswa(
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+) 
