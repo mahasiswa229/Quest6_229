ee0d0e5eb22f42183e260029a526110fbd0ffcd6
mahasiswa229 <mafaqih1945@gmail.com>
2025-11-20 15:16:00 +0700

    feat: Add TampilSiswa composable
    Adds the `TampilSiswa` composable function. This function takes a `Siswa` object representing the UI status and an `onBackButtonClicked` callback.

--- app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
@@ -2,3 +2,9 @@ package com.example.myarsitektur.view
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+fun TampilSiswa(
+    statusUiSiswa: Siswa,
+    onBackButtonClicked: () -> Unit
+) {
+    
+}
